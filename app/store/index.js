d8b8317d9a396484614474185bca73cce1023a65
Aaron Arinder <aaronarinder@gmail.com>
2018-11-09 09:41:05 -0500

    store: adding date, context to DB; dropping dispositions for dev work

--- app/store/index.js
+++ app/store/index.js
@@ -18,23 +18,26 @@ const store = new Vuex.Store({
       state.data = [];
       for(var i = data.data.length - 1; i >= 0; i--){
         state.data.push({
-          disposition: data.data[i]
+          disposition: data.data[i][0],
+          context: data.data[i][1],
+          date: data.data[i][2]
         });
       }
     },
     save(state, data){
       state.data.push({
-        disposition: data.data.disposition
+        disposition: data.data.disposition,
+        context: data.data.context,
+        date: data.data.date,
       });
     },
-  },
-  getters: {
-    getCount: state => state.count,
   },
   actions: {
     init(context){
       (new sqlite('my.db')).then(db => {
-        db.execSQL("CREATE TABLE IF NOT EXISTS dispositions (id INTEGER PRIMARY KEY AUTOINCREMENT, disposition TEXT)").then(id => {
+        // clean db for each dev run
+        db.execSQL("DROP TABLE IF EXISTS dispositions");
+        db.execSQL("CREATE TABLE IF NOT EXISTS dispositions (id INTEGER PRIMARY KEY AUTOINCREMENT, disposition TEXT, context TEXT, date TEXT)").then(id => {
           context.commit('init', { database: db });
         }, err => {
           console.log('table creation error', err);
@@ -44,14 +47,14 @@ const store = new Vuex.Store({
       })
     },
     insert(context, data){
-      context.state.database.execSQL("INSERT INTO dispositions (disposition) VALUES (?)", [data.disposition]).then(id => {
+      context.state.database.execSQL("INSERT INTO dispositions (disposition, context, date) VALUES (?,?,?)", [data.disposition, data.context, new Date()]).then(id => {
         context.commit("save", { data: data });
       }, err => {
         console.log('insert err', err);
       });
     },
     query(context){
-      context.state.database.all("SELECT disposition FROM dispositions", []).then(res => {
+      context.state.database.all("SELECT disposition, context, date FROM dispositions", []).then(res => {
         context.commit("load", { data: res });
       }, err => {
         console.log('select err', err);
